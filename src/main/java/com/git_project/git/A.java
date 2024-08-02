d7c86c30a1cb7365b6f62cc3fd92b92b40bd9e78
vishal kumar <vishalkumar57576@gmail.com>
2024-08-02 10:29:34 +0530

    defect fixed- 3576 fixed null values not to be store in database

--- src/main/java/com/git_project/git/A.java
+++ src/main/java/com/git_project/git/A.java
@@ -1,4 +1,7 @@
 package com.git_project.git;
 
 public class A {
+    public static void main(String[] args) {
+        System.out.println("heloo world");
+    }
 }
---
+++ src/main/java/com/git_project/git/B.java
@@ -0,0 +1,7 @@
+package com.git_project.git;
+
+public class B {
+    public void test(){
+        System.out.println(100);
+    }
+}
